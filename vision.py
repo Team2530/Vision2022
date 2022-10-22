10d8912f1227deb694a253ed4f8464eb647baf67
NifleySnifley <sailzforjunkmailz@gmail.com>
2022-10-21 19:43:57 -0500

    Camera finding

--- vision.py
+++ vision.py
@@ -1,8 +1,17 @@
 import cv2
 import numpy as np
-import sys
+import sys, re, subprocess
 
-cam1 = cv2.VideoCapture("/dev/video6", cv2.CAP_V4L2) if sys.platform == 'linux' else cv2.VideoCapture(1, cv2.CAP_DSHOW)
+CAM_RXP = re.compile(r'(.+) .+:\n\s{8}(.+)')
+
+# Finds the (/dev/videoX) device file of all cameras with a name including `camname`
+def find_cameras(camname):
+    output = subprocess.run(['v4l2-ctl', '--list-devices'], stdout=subprocess.PIPE).stdout.decode('utf-8').replace('\r\n', '\n')
+    devices = {k:v for k,v in CAM_RXP.findall(output)}
+    cams = {k:v for k,v in devices.items() if camname in k}
+    return list(cams.values())
+
+cam1 = cv2.VideoCapture(find_cameras("LifeCam")[0], cv2.CAP_V4L2) if sys.platform == 'linux' else cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
 if not cam1.isOpened():
     print("Error opening camera")
